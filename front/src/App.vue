a044f8953ccfb1e06706e79d914118421289dd49
Tolper4161 <arseniy.z2006@gmail.com>
2023-10-01 21:04:42 +0300

    text edited

--- front/src/App.vue
+++ front/src/App.vue
@@ -7,7 +7,7 @@ import FileForm from './components/FileForm.vue';
 <template>
   <header id="header">
   <nav id="top-navbar">
-      <h1 id="h1-title">ТекстоСховище</h1>
+      <h1 id="h1-title">TextImageSqueeze</h1>
       <div class="top-navbar-links desktop">
           <a href="#advantages-link">Преимущества сервиса</a>
           <a href="#about-project">О проекте</a>
@@ -27,7 +27,7 @@ import FileForm from './components/FileForm.vue';
 <section id="advantages" class="angles">
   <div class="container">
       <div class="advantages-title-block">
-          <h2 class="advantages-title line">Преимущества</h2>
+          <h2 class="advantages-title line">Преимущества сервиса</h2>
       </div>
       <div class="advantages-content">
           <div class="advantages-card">
@@ -37,13 +37,13 @@ import FileForm from './components/FileForm.vue';
           </div>
           <div class="advantages-card">
               <img class="icon" src="/template/img/time-management.png" alt="">
-              <p class="advantages-big-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident...</p>
-              <p class="advantages-small-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+              <p class="advantages-big-text">Нейросеть, используемая сайтом, достаточно обучена, чтобы обработка изображений занимала оптимальное количество времени. Переобучение нейронной сети наоборот, замедлило бы данный процесс. Кроме того, все функции сайта, связанные с данной задачей, максимально оптимизированы, что также способствует ускорению работы. </p>
+              <p class="advantages-small-text">Нейросеть, используемая сайтом, достаточно обучена, чтобы обработка изображений занимала оптимальное количество времени. Все функции, связанные с данной задачей, оптимизированы, что также способствует ускорению работы.</p>
           </div>
           <div class="advantages-card">
               <img class="icon" src="/template/img/pendrive.png" alt="">
-              <p class="advantages-big-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident...</p>
-              <p class="advantages-small-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+              <p class="advantages-big-text">Сжатие изображений происходит циклично (число циклов определяется нейросетью автоматически), пока погрешность сжатия не окажется ниже 2%. Данный подход позволяет получать файлы, занимающие гораздо меньше памяти. Кроме того, наш сайт позволяет преобразовывать изображения к документам с расширением .rtf (Rich Text Format).</p>
+              <p class="advantages-small-text">Сжатие изображений происходит циклично, пока погрешность не станет ниже 2%, что позволяет получать файлы, занимающие меньше памяти. Также можно преобразовать файлы к документам с расширением .rtf.</p>
           </div>
       </div>
   </div>
@@ -51,8 +51,9 @@ import FileForm from './components/FileForm.vue';
 <section id="about-project">
   <div class="container">
       <div class="about-left-col">
-          <h2 class="about-left-col-title">Lorem ipsum dolor?</h2>
-          <p class="line-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
+          <h2 class="about-left-col-title">О проекте</h2>
+          <p class="line-text">К настоящему моменту в мире накоплены большие объемы текстовых неоцифрованных данных. Представляется желанным оцифровывать большое количество информации, которая имеется в виде изображений с текстом. Ввиду того, что получение изображений и распознавание текста являются разными по уровню и требованию задачами, возникает промежуточная проблема - хранение большого количества изображений с текстом, для его дальнейшего распознавания, в сжатом виде с целью экономии места.
+            <br>Сайт создан с целью решения данных проблем. Созданное нашей командой API использует нейронную сеть - данное решение наиболее современное и гибкое. Таким образом, происходит сжатие изображений с текстом без потери информации.</p>
       </div>
       <div class="about-right-col">
           <div class="line-img"></div>
@@ -67,7 +68,7 @@ import FileForm from './components/FileForm.vue';
                   <h3>Социальные сети</h3>
                   <a>Whatsapp</a>
                   <a>Vkontakte</a>
-                  <a>Telegramm</a>
+                  <a>Telegram</a>
                   <a>Discord</a>
               </div>
               <div class="footer-col">
@@ -90,7 +91,7 @@ import FileForm from './components/FileForm.vue';
                   <h3>Помощь</h3>
                   <a>Сайт завис</a>
                   <a>Кнопка "скачать" не работает</a>
-                  <a>Elit sed do </a>
+                  <a>Изображения не сжимаются</a>
                   <a>Техподдержка</a>
               </div>
           </div>
@@ -101,7 +102,7 @@ import FileForm from './components/FileForm.vue';
           <a href="#" id="telegram" class="min-icon"></a>
           <a href="#" id="discord" class="min-icon"></a>
       </div>
-      <h4 class="footer-created-by">Created by Lorem Ipsum</h4>
+      <h4 class="footer-created-by">Создано командой "Кибер-Викинги"</h4>
   </div>
 </footer>
 </template>
